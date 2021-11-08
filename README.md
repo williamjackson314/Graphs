66dd12382ae7664594e4999896ccc770069d4657
williamjackson314 <wtjackson314@gmail.com>
2021-11-07 21:21:33 -0500

    Add fuctions to read file, construct adjacency list, and print out a graph structure from an adjacency list. Also, update the README and fix typo in RomaniaEdges file.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
+# Data Formatting
+- Vertices :
+    * Data shall be in csv format, with only one column containing the name of each vertex
+- Edges :
+    * Data shall be in csv format, with three columns, representing directed edges:
+        starting node, descendent node, edge weight
+
 # Graphs
 Simple implementation of graph structure and related functions
+
+
--- main.py
+++ main.py
@@ -1,6 +1,48 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov  3 21:58:52 2021
+import csv
 
-@author: wtjac
-"""
+''' Support Functions '''    
+def read_file(file_name) :
+
+    with open(file_name, 'r') as file:
+        reader = csv.reader(file)
+        data = list(reader)
+
+    return data
+
+
+def build_adj_list(vertices, edges) :
+    
+    adj_list = {}
+    
+    for vertex in vertices :
+        adj_list[vertex[0]] = []
+        
+    for edge in edges :
+        adj_list[edge[0]].append(tuple(edge))
+        
+    return adj_list
+
+def print_graph(adj_list) :
+
+    for vertex, edges in adj_list.items() :
+        print(str(vertex) + " -> ", end='')
+        for edge in edges :
+            print(str(edge) + ", ", end='')
+        print('\n')
+
+def main() :
+
+    # read in vertices of graph
+    vertices = read_file("RomaniaVertices.txt")
+
+    # Read in edges of graph
+    edges = read_file("RomaniaEdges.txt")
+
+    # construct graph using Romania data
+    romania_graph = build_adj_list(vertices, edges)
+    print_graph(romania_graph)
+
+
+    return None
+
+main()
