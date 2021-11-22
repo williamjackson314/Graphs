629b3482a72e83eb14adfe176adf37f64f1b22a5
williamjackson314 <wtjackson314@gmail.com>
2021-11-22 14:52:26 -0500

    Finish first implementation of dijkstras algorithm and add a print path function.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import csv
-from queue import Queue
-import distutils
+import queue as q
+import heapq
 
 ''' Support Functions '''    
 def read_csv_file(file_name) :
@@ -23,10 +23,10 @@ def read_csv_file(file_name) :
 
 class Vertex :
     
-    def __init__(self, name, color, distance, pi) :
+    def __init__(self, name, color, d, pi) :
         self.name = name
         self.color = color
-        self.d = distance
+        self.d = d
         self.pi = pi
 
 ''' Adjacency List Functions '''
@@ -51,9 +51,9 @@ def build_adj_list(vertices, edges) :
         adj_list[vertex] = []
 
     for edge in edges :
-        adj_list[edge[0]].append(tuple(edge))
-
-    print(adj_list)    
+        adj_list[edge[0]].append((edge[1], edge[2]))
+        adj_list[edge[1]].append((edge[0], edge[2]))
+  
     return adj_list
 
 def print_graph(adj_list) :
@@ -73,72 +73,59 @@ def print_graph(adj_list) :
             print(str(edge) + ", ", end='')
         print('\n')
 
-
-def get_adjacent_vertices(adj_list, vertex) :
+def breadth_first_search(adj_list, source_vertex_name) :
     """
 
     Parameters:
         param adj_list: adjacency list
         type adj_list: dictionary
         
-        param vertex: adjacent vertices found relative to this vertex
-        type vertex: Vertex object
-
-    Returns:
-        adj_vertices: list of names of vertices adjacent to the vertex parameter
-
-    """
-    
-    adj_vertices = []
-    for edge in adj_list[vertex] :
-        adj_vertices.append(edge[1])
-    
-    return adj_vertices
-
-def breadth_first_search(adj_list, source_vertex) :
-    """
-
-    Parameters:
-        param adj_list: adjacency list
-        type adj_list: dictionary
-        
-        param vertex: vertex where search begins
-        type vertex: Vertex object
+        param vertex_name: vertex where search begins
+        type vertex_name: Vertex object
 
     Returns: 
         None
 
     """
-    
-    Q = Queue()
+
+    Q = []
     vertices = {}
 
     for vertex in adj_list.keys() :
         vertices[vertex] = Vertex(name = vertex, color = "White", d = float("inf"), pi = None)
 
-    source_vertex.color = "White"
-    source_vertex.d = 0
-    source_vertex.pi = None
+    vertices[source_vertex_name].d = 0
 
-    Q.put(source_vertex)
-    while Q.empty() is not True :
-        u = Q.get()
+    Q.append(vertices[source_vertex_name])
+    while len(Q) > 0:
+        u = Q.pop()
 
-        adj_vertices = get_adjacent_vertices(adj_list, u.name)
-        for vertex in adj_vertices :
-            v = vertices[vertex]
+        for edge in adj_list[u.name]:
+
+            # edge[0] = name of adjacent vertex
+            # edge[1] = edge weight between vertices
+            v = vertices[edge[0]]
             if v.color == "White" :
                 v.color = "Gray"
                 v.d = u.d + 1
                 v.pi = u.name
-                Q.put(v)
+                Q.append(v)
 
         u.color = "Black"
 
-    return None
+    return vertices
 
 ''' Dijkstra's Algorithm Functions '''
-def init(adj_list, src_vertex) :
+def print_path(vertex_list, src_vertex, dest_vertex):
+    
+    predecessor = vertex_list[dest_vertex.pi]
+    print(dest_vertex.name + " <- ", end='')
+    while (predecessor.name != src_vertex.name):
+        print(predecessor.name + " <- ", end='')
+        predecessor = vertex_list[predecessor.pi]
+    print(src_vertex.name)
+        
+def init(adj_list, src_vertex_name) :
     """    
     
     Parameters:
@@ -149,14 +136,16 @@ def init(adj_list, src_vertex) :
         type src_vertex: Vertex object
 
     Returns:
-        None
+        vertices : a dictionary with vertex name as key and Vertex object as value
 
     """
     
     vertices = {}
     for vertex in adj_list :
         vertices[vertex] = Vertex(name = vertex, color = None, d = float("inf"), pi = None)
-    src_vertex.d = 0
+    vertices[src_vertex_name].d = 0
+    
+    return vertices
     
 def relax(src_vertex, dest_vertex, edge_weight) :
     """
@@ -180,47 +169,83 @@ def relax(src_vertex, dest_vertex, edge_weight) :
         dest_vertex.d = src_vertex.d + edge_weight
         dest_vertex.pi = src_vertex.name
 
-def dijkstras(adj_list, edge_weight, src_vertex) :
+def dijkstras_algorithm(adj_list, src_vertex_name) :
     """
-    
 
     Parameters:
         param adj_list: adjacency list
         type adj_list: dictionary
         
-        param edge_weight: weight of the edge between ______
-        type edge_weight: number
-        
-        param src_vertex: the vertex where the algorithm starts
-        type src_vertex: Vertex object
+        param src_vertex_name: the vertex where the algorithm starts
+        type src_vertex_name: Vertex object
 
     Returns:
         None
 
     """
-    init(adj_list, src_vertex)
-    vertex_set = None
+    # initializes a dictionary of the form vertex_name : Vertex object
+    vertices = init(adj_list, src_vertex_name)
+
+    path_known = set(()) # set of vertices whose shortest path is known
+    Q = [] # initialize priority queue
+    for vertex in adj_list.keys() :
+        vertex_tuple = (vertices[vertex].d, vertex) # build priority queue item
+        heapq.heappush(Q, vertex_tuple) # add item to priority queue
     
-    return None
+    while len(Q) > 0 :
+        u = heapq.heappop(Q)[1] # retrieve second value in priority queue tuple: vertex name
+        path_known.add(u)
+        
+        #print(vertices[u].d)
+        
+        # edges in the adjacency list dictionary at key == u
+        for edge in adj_list[u] :
+            
+            # edge[0] = name of adjacent vertex
+            # edge[1] = edge weight between vertices
+            if edge[0] not in path_known :
+                
+                relax(vertices[u], vertices[edge[0]], int(edge[1]))
+                
+                # Add tuple with adjusted d value to priority queue
+                vertex_tuple = (vertices[edge[0]].d, edge[0])
+                heapq.heappush(Q, vertex_tuple)
+
+    return vertices
 
 ''' Main Function '''
 def main() :
 
-    # help(function_name): returns docstring for function function_name
-    
     # read in vertices of graph
     vertices = read_csv_file("RomaniaVertices.txt")
     vertices = [x[0] for x in vertices]
 
     # Read in edges of graph
     edges = read_csv_file("RomaniaEdges.txt")
-    #print(edges)
     
     # construct graph using Romania data
     romania_graph = build_adj_list(vertices, edges)
-    #print_graph(romania_graph)
+    
+    print("1: BFS\n2: Print Graph\n3: Dijkstras\n4: Quit")
+    user_input = int(input())
+    while user_input != 4 :
+        if user_input == 1 :
+            vertices = breadth_first_search(romania_graph, "Arad")
+            print_path(vertices, vertices["Arad"], vertices["Sibiu"])
+            print_path(vertices, vertices["Arad"], vertices["Craiova"])
+            print_path(vertices, vertices["Arad"], vertices["Bucharest"])
+        elif user_input == 2 :
+            print_graph(romania_graph)
+        elif user_input == 3 :
+            vertices = dijkstras_algorithm(romania_graph, "Arad")
+            print_path(vertices, vertices["Arad"], vertices["Bucharest"])
+        else:
+            print("Invalid choice, try again: ")
+        
+        print("1: BFS\n2: Print Graph\n3: Dijkstras\n4: Quit")
+        user_input = int(input())
 
-
+    
     return None
 
 main()
